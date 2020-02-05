a7980b800552cedab6b48500741515e31a36f6a6
Ygor Nascimento <ygor.nascimento@outlook.com>
2020-02-05 13:55:22 -0300

    Firebase Configured

--- Grocr/Grocr/AppDelegate.swift
+++ Grocr/Grocr/AppDelegate.swift
@@ -21,14 +21,20 @@
  */
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
   
+  override init() {
+    FirebaseApp.configure()
+  }
+  
   private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]? = [:]) -> Bool {
     UIApplication.shared.statusBarStyle = .lightContent
+    
     return true
   }
 
